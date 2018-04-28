0912ddf6ae91f95c9df2b9e7eb2d525fe68b8bcf
securekim <35220663+securekim@users.noreply.github.com>
2018-04-29 00:15:41 +0900

    P2P Error Handling in Websocket #17

--- src/p2p.js
+++ src/p2p.js
@@ -15,12 +15,23 @@ const startP2PServer = server => {
 
 const initSocketConnection = socket =>{
     sockets.push(socket);
+    handleSocketError(socket);
     socket.on("message",(data) => {
         console.log(data);
     });
     setTimeout(()=>{
         socket.send("welcome");
     },5000);
+}
+
+const handleSocketError = ws =>{
+    const closeSocketConnection = ws => {
+        ws.close();
+        //for dead socket
+        sockets.splice(sockets.indexOf(ws),1); // WOW !!!!!! AWESOME
+    };
+    ws.on("close", ()=>closeSocketConnection(ws));
+    ws.on("error", ()=>closeSocketConnection(ws));
 }
 
 const connectToPeers = newPeer => {
