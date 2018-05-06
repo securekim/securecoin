6e66126f33b2cbe3eeb43e0876f8884b1b410403
securekim <35220663+securekim@users.noreply.github.com>
2018-05-06 12:42:15 +0900

    Getting the Transaction ID #34

--- src/transaction.js
+++ src/transaction.js
@@ -1,3 +1,5 @@
+const CryptoJS = require("crypto-js");
+
 class TxOut {
     constructor(address, amount){
         this.address = address;
@@ -34,3 +36,24 @@ class uTxOut{
 }
 
 let uTxOuts = [];
+
+//transaction will be hashed -> This is ID
+
+const getTxId = tx => {
+
+    // 각 배열에 있는 값들을 Serialize 해서 String 으로 만듬.
+
+    const txInContent = tx.txIns
+        .map(txIn => txIn.uTxOutId + txIn.txOutIndex)
+        .reduce((a,b) => a + b, "" );
+    
+    const txOutContent = tx.txOuts
+        .map(txOut => txOut.address + txOut.amount)
+        .reduce((a,b) => a + b, "");
+    
+
+    return CryptoJS.SHA256(txInContent + txOutContent).toString(); 
+}
+
+
+
