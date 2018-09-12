744c3656dc7b4c6ef7c8f07ce2315875344082b7
securekim <admin@securekim.com>
2018-09-13 00:30:33 +0900

    Getting Address and Address Balance #51

--- src/wallet.js
+++ src/wallet.js
@@ -1,6 +1,7 @@
 const elliptic = require("elliptic"),
 path = require("path"),    
   fs = require("fs");
+  _  = require("lodash");
 
 const ec = new elliptic.ec("secp256k1");
 
@@ -12,6 +13,24 @@ const generatePrivateKey = () => {
     return privateKey.toString(16);
 }
 
+const getPrivateFromWallet = () => {
+    const buffer = fs.readFileSync(privateKeyLocation, "utf-8");
+    buffer.toString();
+};
+
+const getPublicFromWallet = () => {
+    const privateKey = getPrivateFromWallet();
+    const key = ec.keyFromPrivate(privateKey,"hex");
+    return key.getPublic().encode("hex");
+}
+
+const getBalance = (address,uTxOuts) => {
+    return _(uTxOuts)
+    .filter(uTxO => uTxO.address === address)
+    .map(uTxO => uTxO.amount)
+    .sum(); // reduce 해도 되는데 lodash 가 더 짧다.
+}
+
 const initWallet = () => {
     if (fs.existsSync(privateKeyLocation)){
         console.log("Wallet is Already Exist.");
@@ -19,7 +38,7 @@ const initWallet = () => {
     }
     const newPrivateKey = generatePrivateKey();
     fs.writeFileSync(privateKeyLocation, newPrivateKey);
-}
+};
 
 module.exports = {
     initWallet
